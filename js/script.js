6393e1868b611f6a5123bc1bb0fbf51875050d12
Isaac <uniqucode@gmail.com>
2022-08-12 21:22:04 +0100

    Refrencing saveConfirmed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 const resultsNav = document.getElementById('resultsNav');
 const favoritesNav = document.getElementById('favoritesNav');
 const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
