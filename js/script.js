881e48380c42eaba66075c9444ae4cad82f94692
Isaac <uniqucode@gmail.com>
2022-08-12 21:23:29 +0100

    Refrencing loader

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ const resultsNav = document.getElementById('resultsNav');
 const favoritesNav = document.getElementById('favoritesNav');
 const imagesContainer = document.querySelector('.images-container');
 const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
