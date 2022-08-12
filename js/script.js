e1ae26632b03dd53650d8ebe7c33936943c94e50
Isaac <uniqucode@gmail.com>
2022-08-12 21:17:43 +0100

    Refrencing imagesContainer

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
 const resultsNav = document.getElementById('resultsNav');
 const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
