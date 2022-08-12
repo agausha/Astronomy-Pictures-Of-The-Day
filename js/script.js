56b3dba5989fe6d804e68d984afa0e4b1abadddc
Isaac <uniqucode@gmail.com>
2022-08-12 21:45:38 +0100

    Added image count

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ const favoritesNav = document.getElementById('favoritesNav');
 const imagesContainer = document.querySelector('.images-container');
 const saveConfirmed = document.querySelector('.save-confirmed');
 const loader = document.querySelector('.loader');
+
+// NASA API
+const count = 20;
